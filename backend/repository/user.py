26d54edfdf450cb46619f4e3c1684d1931a48f89
Ershov Dariy <ershovdar@yandex.ru>
2025-12-06 13:18:10 +0300

    fix users add controllers auth

--- backend/repository/user.py
+++ backend/repository/user.py
@@ -1,6 +1,6 @@
+from uuid import UUID
 from sqlalchemy import select, func, delete
 from sqlalchemy.ext.asyncio import AsyncSession
-
 from model.user import User
 
 
@@ -8,23 +8,21 @@ class UserRepository:
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def get_by_id(self, id: int) -> User | None:
+    async def get_by_id(self, id: UUID) -> User | None:
         result = await self.session.execute(select(User).where(User.id == id))
         return result.scalar_one_or_none()
 
     async def create(self, user: User) -> User:
         self.session.add(user)
         await self.session.commit()
+        await self.session.refresh(user)
         return user
+
 
     async def update(self, user: User) -> User:
         self.session.add(user)
         await self.session.commit()
         return user
-
-    async def delete(self, id: int) -> None:
-        await self.session.execute(delete(User).where(User.id == id))
-        await self.session.commit()
 
     async def get_all(self) -> list[User]:
         result = await self.session.execute(select(User))
@@ -33,6 +31,10 @@ class UserRepository:
     async def count(self) -> int:
         result = await self.session.execute(select(func.count(User.id)))
         return result.scalar_one()
+
+    async def delete(self, id: UUID) -> None:  # ← UUID
+        await self.session.execute(delete(User).where(User.id == id))
+        await self.session.commit()
 
     async def get_by_email(self, email: str) -> User | None:
         result = await self.session.execute(select(User).where(User.email == email))
