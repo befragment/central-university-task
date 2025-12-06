4a2eb506b4f203aed84da2f5f5f496c4bef8d57f
Нистратов Степан Сергеевич <s.nistratov@kscgroup.ru>
2025-12-06 15:41:26 +0300

    fixed seed

--- seed/01_users.sql
+++ seed/01_users.sql
@@ -6,12 +6,14 @@
 -- | maria@example.com  | password123  |
 -- | alex@example.com   | password123  |
 -- ============================================
--- Hash: bcrypt, cost=12
 
 INSERT INTO "user" (id, created_at, name, email, pass_hash)
-VALUES ('11111111-1111-1111-1111-111111111111', '2025-01-10', 'Иван Петров', 'ivan@example.com',
-        '$2b$12$LQv3c1yqBWVHxkd0LHAkCOYz6TtxMQJqhN8/X4.VTtYA0wQQVQJhC'),
-       ('22222222-2222-2222-2222-222222222222', '2025-01-11', 'Мария Сидорова', 'maria@example.com',
-        '$2b$12$LQv3c1yqBWVHxkd0LHAkCOYz6TtxMQJqhN8/X4.VTtYA0wQQVQJhC'),
-       ('33333333-3333-3333-3333-333333333333', '2025-01-12', 'Алексей Козлов', 'alex@example.com',
-        '$2b$12$LQv3c1yqBWVHxkd0LHAkCOYz6TtxMQJqhN8/X4.VTtYA0wQQVQJhC') ON CONFLICT (id) DO NOTHING;
+VALUES
+    ('71038400-4523-43f3-8f2c-38722d0c4c89', '2025-01-10', 'Иван Петров', 'ivan@example.com',
+     '$2b$12$fQ722.ymNPXRNLyj5fiPn.HAKJqvdrPVwToucDdvrDgVeLuJtumBe'),
+    ('a450092b-15e7-4283-9710-7be1ed7886b0', '2025-01-11', 'Мария Сидорова', 'maria@example.com',
+     '$2b$12$fQ722.ymNPXRNLyj5fiPn.HAKJqvdrPVwToucDdvrDgVeLuJtumBe'),
+    ('efee247e-4350-483a-bb51-09453662ad07', '2025-01-12', 'Алексей Козлов', 'alex@example.com',
+     '$2b$12$fQ722.ymNPXRNLyj5fiPn.HAKJqvdrPVwToucDdvrDgVeLuJtumBe')
+ON CONFLICT (id) DO NOTHING;
+
